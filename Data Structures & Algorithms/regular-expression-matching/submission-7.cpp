ab16f9179b9e3042d458ceb7dc92f6b4d8e6e72c
Sidhant Uppal <145711961+OutLawyer96@users.noreply.github.com>
2026-07-10 16:40:44 +0530

    Add: regular-expression-matching - submission-7

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-7.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool matcher(string& s, string& p, int i, int j, vector<vector<int>>& dp) {
+        if (j == p.size()) {
+            return i == s.size();
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        bool val=false;
+        if (i == s.size()) {
+            if (p[j] == '*') {
+                return matcher(s, p, i, j + 1,dp);
+            }
+
+            if (j + 1 < p.size() && p[j + 1] == '*') {
+                return matcher(s, p, i, j + 1,dp);
+            }
+
+            return false;
+        }
+
+        if (j + 1 < p.size() && p[j + 1] == '*') {
+            val= matcher(s, p, i, j + 1,dp);
+        }
+
+        else if (s[i] == p[j] || p[j] == '.') {
+            val= matcher(s, p, i + 1, j + 1,dp);
+        }
+        else if (p[j] == '*') {
+            bool res = false;
+
+            res = res || matcher(s, p, i, j + 1,dp);
+
+            if (s[i] == p[j - 1] || p[j - 1] == '.') {
+                res = res || matcher(s, p, i + 1, j,dp);
+            }
+
+            val= res;
+        }
+        if(val){
+            dp[i][j]=1;
+        }else{
+            dp[i][j]=0;
+        }
+        return val;
+    }
+
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size()+1,vector<int> (p.size()+1,-1));
+        return matcher(s, p, 0, 0,dp);
+    }
+};
