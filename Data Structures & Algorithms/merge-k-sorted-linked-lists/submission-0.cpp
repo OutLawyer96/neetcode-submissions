2b117f59da276178ade2b8b908d70838183f8c84
Sidhant Uppal <145711961+OutLawyer96@users.noreply.github.com>
2026-07-27 12:44:08 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+struct Compare {
+    bool operator()(pair<ListNode*,int> a, pair<ListNode*,int> b) {
+        return a.first->val > b.first->val;  
+    }
+};
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<ListNode*,int>,vector<pair<ListNode*,int>>,Compare> pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]){
+                pq.push({lists[i],i});
+                lists[i]=lists[i]->next;
+            }
+        }
+        if(pq.size()==0){
+            return nullptr;
+        }
+        ListNode* front=pq.top().first;
+        int k=pq.top().second;
+        if(lists[k]){
+            pq.push({lists[k],k});
+            lists[k]=lists[k]->next;
+        }
+        ListNode* curr=front;
+        pq.pop();
+        while(!pq.empty()){
+            auto now=pq.top();
+            pq.pop();
+            curr->next=now.first;
+            curr=curr->next;
+            if(lists[now.second]){
+                pq.push({lists[now.second],now.second});
+                lists[now.second]=lists[now.second]->next;
+            }
+        }
+        return front;
+    }
+};
