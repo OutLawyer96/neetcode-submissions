1d87ee34ffe8b7cc964e4de7a2fe0dc8f0c98d4e
Sidhant Uppal <145711961+OutLawyer96@users.noreply.github.com>
2026-08-03 14:56:47 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        if(n>0){
+            if(n%2==0){
+                return myPow(x,n/2)*myPow(x,n/2);
+            }else{
+                return myPow(x,n/2)*myPow(x,n/2)*x;
+            }
+        }else{
+            return myPow(1/x,n*-1);
+        }
+    }
+};
